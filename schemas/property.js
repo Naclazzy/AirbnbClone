d795d599cf569e7415ec4cf2a17209f8f7cf8540
JANWE <jan.wegmann@hotmail.de>
2022-04-24 04:26:08 +0200

    add data to schema

--- schemas/property.js
+++ schemas/property.js
@@ -7,6 +7,68 @@ export default{
             name: 'title', 
             title: 'Title',
             type: 'string' 
-        }
+        },
+        {
+            name: 'location', 
+            title: 'Location',
+            type: 'geopoint' 
+        },
+        {
+            name: 'propertyType', 
+            title: 'Property Type',
+            type: 'string',
+            options: {
+                list:[
+                    { title: 'House', value:'house'},
+                    { title: 'Flat', value: 'flat'},
+                    { title: 'Bed and Breakfast', value: 'bed-and-breakfast'},
+                    { title: 'Boutique Hotel', value: 'boutique-hotel'}
+                ],
+                layout:'radio'
+            } 
+        },
+        {
+            name: 'mainImage',
+            title: 'Main Image',
+            type: 'image',
+            option: {
+                hotspot: true,
+                    },
+        },
+        // {
+        //     name:'images', 
+        //     title:'Images', 
+        //     type: 'array',
+        //     of: [{type:'propertyImage'}]
+        // },
+        {
+            name: 'pricePerNight', 
+            title: 'Price Per Night',
+            type: 'number' 
+        }, 
+        {
+            name:'bedrooms', 
+            title:'Bedrooms', 
+            type: 'number'  
+        }, 
+        {
+            name:'slug', 
+            title:'Slug', 
+            type: 'slug',
+            options:{
+                source: 'title',
+                maxlength: '100',
+            },  
+        }, 
+        {
+            name:'id', 
+            title:'ID', 
+            type: 'number'  
+        }, 
+        {
+            name:'description', 
+            title:'Description', 
+            type: 'string'  
+        },  
     ]
 }
